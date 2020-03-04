f93a366dd95f46b549c1f71d5115cbd8acf1b427
alanqq0624 <alanqq0624@hotmail.com>
2020-03-04 22:56:00 +0800

    Add basic test of Vue.js 	add attribute "defer" in script to make script.js execute at the end of HTML

---
+++ public/script.js
@@ -0,0 +1,27 @@
+var cow2 = new Vue({
+    el: '#cow2',
+    data: {
+        name: "cow2",
+        ID: 2,
+        time: '2020-03-01 17-01-23',
+        Ambient: 24,
+        Object: 24,
+        Walk: 2,
+        Ruminating: 5,
+        Eat_time: 1
+    }
+})
+
+var cow3 = new Vue({
+    el: '#cow3',
+    data: {
+        name: "cow3",
+        ID: 3,
+        time: '2020-03-04 22-26-55',
+        Ambient: 33,
+        Object: 33,
+        Walk: 3,
+        Ruminating: 8,
+        Eat_time: 2
+    }
+})
