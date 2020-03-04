71e29f02f9064ac5c84b3c80266c41742d11591a
alanqq0624 <alanqq0624@hotmail.com>
2020-03-05 01:05:15 +0800

    Beautify the website with W3.CSS 	"Make things eaiser"

--- public/script.js
+++ public/script.js
@@ -1,3 +1,18 @@
+var cow1= new Vue({
+    el: '#cow1',
+    data: {
+        name: "cow1",
+        ID: 1,
+        time: '2020-02-28 15-01-43',
+        Ambient: 12,
+        Object: 12,
+        Walk: 1,
+        Ruminating: 4,
+        Eat_time: 2
+    }
+})
+
+
 var cow2 = new Vue({
     el: '#cow2',
     data: {
@@ -23,5 +38,19 @@ var cow3 = new Vue({
         Walk: 3,
         Ruminating: 8,
         Eat_time: 2
+    }
+})
+
+var cow4 = new Vue({
+    el: '#cow4',
+    data: {
+        name: "cow4",
+        ID: 4,
+        time: '2020-03-04 22-26-55',
+        Ambient: 215,
+        Object: 34,
+        Walk: 9,
+        Ruminating: 3,
+        Eat_time: 1
     }
 })
