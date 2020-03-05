21537a0ad91cf984c7b60995f79ed88b54b244a5
alanqq0624 <alanqq0624@hotmail.com>
2020-03-06 01:22:59 +0800

    Beautify the html with color and margin

--- public/script.js
+++ public/script.js
@@ -17,32 +17,42 @@ Vue.component('cow_card', {
     ],
     template: `
         <div class="w3-half w3-panel">
-            <div class="cow_block w3-card w3-padding w3-round-large">
-                <div class="cow_name w3-section w3-row">
+            <div class="cow_block w3-card w3-padding w3-round w3-theme-l3">
+                <div class="cow_name w3-section w3-row w3-padding w3-round-large w3-theme-d5">
                     <h3>CID: {{cid}}</h3>
                 </div>
                 <div class="w3-row-padding w3-section">
-                    <div class="ambient_temp w3-col l6 m6 s6">
-                        Ambient:<br />
-                        {{ambient}} ℃
+                    <div class="w3-col l6 m6 s6">
+                        <div class="ambient_temp w3-padding w3-round-large w3-theme-d3">
+                            Ambient:<br />
+                            {{ambient}} ℃
+                        </div>
                     </div>
-                    <div class="cow_temp w3-col l6 m6 s6">
-                        Cow: <br />
-                        {{object}} ℃
+                    <div class="w3-col l6 m6 s6">
+                        <div class="cow_temp w3-padding w3-round-large w3-theme-d3">
+                            Cow: <br />
+                            {{object}} ℃
+                        </div>
                     </div>
                 </div>
                 <div class="w3-row-padding w3-section">
-                    <div class="cow_walk w3-third">
-                        Walk: <br />
-                        {{walk}}
+                    <div class="w3-third w3-section">
+                        <div class="cow_walk w3-padding w3-round-large w3-theme-d2">
+                            Walk: <br />
+                            {{walk}}
+                        </div>
                     </div>
-                    <div class="cow_RU w3-third">
-                        Ruminating: <br />
-                        {{ruminating}}
+                    <div class="w3-third w3-section">
+                        <div class="cow_RU w3-padding w3-round-large w3-theme-d2">
+                            Ruminating: <br />
+                            {{ruminating}}
+                        </div>
                     </div>
-                    <div class="cow_head w3-third">
-                        Eat_time: <br />
-                        {{eat_time}}
+                    <div class="w3-third w3-section">
+                        <div class="cow_head w3-padding w3-round-large w3-theme-d2">
+                            Eat_time: <br />
+                            {{eat_time}}
+                        </div>
                     </div>
                 </div>
             </div>
